7b5265654445f0e371415a7f944108395891db67
Lucas de Moraes Telles <108810631+LucasMTelles@users.noreply.github.com>
2023-04-10 12:26:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+//atividade 1
+
+//Escreva um script no JavaScript para unir todos os elementos do array
+
+/*const numeros = ["um", "dois", "três", "quatro", "cinco"];
+let join = numeros.join("");
+
+console.log(join);*/
+
+
+
+//atividade 2
+
+//Escreva um script que contenha  uma lista de usuários, ordenada por ordem alfabética
+
+/*const usuarios = ["Ana", "Lucia", "Jorge", "Pedro"];
+let ordem = usuarios.sort();
+
+console.log(ordem);*/
+
+
+
+//atividade 3
+
+/*function myfunction(a, b){
+    let array = [];
+
+    if(a.indexOf(b) !== -1){
+        array.push(b + a);
+        return array;
+    } else {
+        array.push(a + b);
+    }
+}*/
+
+
+
+//atividade 4
+
+//Escreva um script que remova o primeiro elemento de um array.
+
+const frutas = ["banana", "mamão", "melancia", "abacaxi", "uva"];
+frutas.shift();
+
+console.log(frutas);
